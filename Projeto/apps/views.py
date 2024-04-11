727f199731c30d218f6206aa456a2652d0a5008c
Maria Júlia Peixoto <mjpo@cesar.school>
2024-04-11 15:55:27 -0300

    ajeitei o css

--- Projeto/apps/views.py
+++ Projeto/apps/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from . models import *
+from django.http import HttpResponse
 
 def home(request):
     return render(request, "apps/home.html")
--- Projeto/virtu/settings.py
+++ Projeto/virtu/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Diretórios de arquivos static
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
