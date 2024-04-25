14a7c4935d72e2d0b6fc79012e4ed574be9104be
Maria Júlia Peixoto <mjpo@cesar.school>
2024-04-25 16:10:13 -0300

    push logout

--- Projeto/apps/urls.py
+++ Projeto/apps/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -8,6 +7,7 @@ urlpatterns = [
     path('cliente_cadastro/', views.cliente_cadastro, name='cliente_cadastro'),  # Cadastro do cliente
     path('servicos/', views.servicos, name='servicos'),  # Página de serviços (funcionário)
     path('funcionario_login/', views.funcionario_login, name='funcionario_login'),  # Login do funcionário
+    path('logout/', views.logout_view, name='logout'),
     path('funcionario_cadastro/', views.funcionario_cadastro, name='funcionario_cadastro'),  # Cadastro do funcionário
     path('home_cliente/', views.home_cliente, name='home_cliente'),  # Cadastro do funcionário
     path('cadastrar_os_cliente/', views.cadastrar_os_cliente, name='cadastrar_os_cliente'),  # Cadastro OS por parte do cliente
