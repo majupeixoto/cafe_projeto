59b0ee128642b1ec0a597ebd6b264b98496ba09d
luana <llmaf@cesar.school>
2024-06-11 20:17:35 -0300

    conclução do teste de lista_OS_func

--- Projeto/cypress/e2e/lista_OS_func.cy.js
+++ Projeto/cypress/e2e/lista_OS_func.cy.js
@@ -12,6 +12,7 @@ describe('teste da visualiazação pelo funcionario da lista das OS’s', () => 
         cy.get('.btn').click();
         cy.get(':nth-child(2) > a > .img-fluid').click();
         cy.wait(1000);
+        cy.get('thead > tr > :nth-child(1)').should('not.exist');
     })
     it('Com OS cadastrada no sistema', () => {
         cy.visit('/');
@@ -25,6 +26,7 @@ describe('teste da visualiazação pelo funcionario da lista das OS’s', () => 
         cy.get('#id_email').type('gabi@gmail.com');
         cy.get('#id_senha').type('123');
         cy.get('#id_confirmar_senha').type('123');
+        cy.wait(1000);
         cy.get('.btn').click();
         cy.get('.btn').click();
         cy.get('.picture').attachFile('imgs/maquina_de_lavar.jpg');
@@ -32,7 +34,20 @@ describe('teste da visualiazação pelo funcionario da lista das OS’s', () => 
         cy.get('#modelo').type('Electrolux');
         cy.get('#descricao_problema').type('Maquina de Lavar não está funcionando');
         cy.get('#garantia_sim').click();
+        cy.wait(1000);
         cy.get('.btn-primary').click();
-
+        cy.get(':nth-child(5) > a > .img-fluid').click();
+        cy.get('[href="/funcionario_login/"]').click();
+        cy.get('a').click();
+        cy.get('#id_nome').type('Nanga Gueiros');
+        cy.get('#id_email').type('nanda@gmail.com');
+        cy.get('#id_username').type('nanda');
+        cy.get('#id_senha').type('123');
+        cy.get('#id_confirmar_senha').type('123');
+        cy.wait(1000);
+        cy.get('.btn').click();
+        cy.get(':nth-child(2) > a > .img-fluid').click();
+        cy.wait(1000);
+        cy.get('thead > tr > :nth-child(1)').should('exist');
     })
 })
