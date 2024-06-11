2fe2e6edae4f5a0b46d01ad82a27abe55af1fae8
luana <llmaf@cesar.school>
2024-06-11 19:08:48 -0300

    teste de lista_OS_func

--- Projeto/cypress/e2e/lista_OS_func.cy.js
+++ Projeto/cypress/e2e/lista_OS_func.cy.js
@@ -8,15 +8,32 @@ describe('teste da visualiazação pelo funcionario da lista das OS’s', () => 
         cy.get('#id_username').type('larissa');
         cy.get('#id_senha').type('123');
         cy.get('#id_confirmar_senha').type('123');
+        cy.wait(1000);
         cy.get('.btn').click();
+        cy.get(':nth-child(2) > a > .img-fluid').click();
+        cy.wait(1000);
     })
     it('Com OS cadastrada no sistema', () => {
         cy.visit('/');
-        cy.get('[href="/funcionario_login/"]').click();
-        cy.get('#username').type('lala');
-        cy.get('#password').type('123')
-        cy.get('.btn').click()
-        cy.url().should('include', 'servicos')
+        cy.get('#cliente > .botao').click();
+        cy.get('a').click();
+        cy.get('#id_nome').type('Gabi Lima');
+        cy.get('#id_username').type('gabi');
+        cy.get('#id_cpf').type('18851022628');
+        cy.get('#id_data_nascimento').type('2005-03-01');
+        cy.get('#id_contato').type('81923456789');
+        cy.get('#id_email').type('gabi@gmail.com');
+        cy.get('#id_senha').type('123');
+        cy.get('#id_confirmar_senha').type('123');
+        cy.get('.btn').click();
+        cy.get('.btn').click();
+        cy.get('.picture').attachFile('imgs/maquina_de_lavar.jpg');
+        cy.get('#aparelho').type('Maquina de Lavar');
+        cy.get('#modelo').type('Electrolux');
+        cy.get('#descricao_problema').type('Maquina de Lavar não está funcionando');
+        cy.get('#garantia_sim').click();
+        cy.get('.btn-primary').click();
+
     })
 
 })
