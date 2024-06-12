cc2a82d10ca99e04e09150602fa8762516def471
paulo-rago <149490954+paulo-rago@users.noreply.github.com>
2024-06-11 22:50:31 -0300

    .

--- Projeto/cypress/e2e/editar_e_acompannhar_os.cy.js
+++ Projeto/cypress/e2e/editar_e_acompannhar_os.cy.js
@@ -43,10 +43,6 @@ describe('acompanhamento status da os pelo cliente', () => {
         cy.get('#ver-mais-lista-os').click();
         cy.get('.btn').click();
         cy.get('.btn').click();
-        cy.get('#mensagem_funcionario').type('Olá, Jonatas! Estamos aguardando peças para realizar seu serviço, daqui algum tempo iremos lhe atualizar da situação!');
-        cy.get('#problema_detectado').type('Por enquanto nada...');
-
-        // Ensure select is visible and interact
         cy.get('#status').should('be.visible').select('Aguardando peça');
         cy.get('#status').should('have.value', 'Aguardando_peca');
         cy.get('#tipo_atendimento').should('be.visible').select('Garantia on site');
