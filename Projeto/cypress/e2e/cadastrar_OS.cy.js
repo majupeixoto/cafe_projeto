c056425a639d3287a1f1be6558e0e224e0dbb788
Maria Júlia Peixoto <mjpo@cesar.school>
2024-06-11 22:36:39 -0300

    add notificacao

--- Projeto/cypress/e2e/cadastrar_OS.cy.js
+++ Projeto/cypress/e2e/cadastrar_OS.cy.js
@@ -1,44 +1,27 @@
-describe('teste cadastro os funcionario', () => {
+describe('teste cadastro os usuário', () => {
     it('teste 1', () => {
         cy.visit('/');
         cy.wait(1000);
         cy.get('#cliente > .botao').click();
         cy.wait(2000);
         cy.get('a').click();
-        cy.get('#id_nome').type('bob');
-        cy.get('#id_username').type('bob');
+        cy.get('#id_nome').type('Maria Alice');
+        cy.get('#id_username').type('alice');
         cy.get('#id_cpf').type('11122233304');
         cy.get('#id_data_nascimento').type('1970-03-21');
         cy.get('#id_contato').type('999998888');
-        cy.get('#id_email').type('bob@gmail.com');
+        cy.get('#id_email').type('alice@gmail.com');
         cy.get('#id_senha').type('123');
         cy.get('#id_confirmar_senha').type('123');
-        cy.get('.btn').click();
         cy.wait(2000);
         cy.get('.btn').click();
+        cy.get(':nth-child(2) > a > .img-fluid').click();
         cy.get('#aparelho').type('microondas');
         cy.get('#descricao_problema').type('aparelho não quer ligar');
         cy.get('#modelo').type('lg 1234')
         cy.get('#garantia_sim').click();
         cy.get('.btn-primary').click();
-        cy.wait(2000)
-        cy.get(':nth-child(5) > a > .img-fluid').click();
-        cy.get('#funcionario > .botao').click();
-        cy.get('a').click();
-        cy.get('#id_nome').type('rob');
-        cy.get('#id_email').type('rob@tech.com')
-        cy.get('#id_username').type('rob');
-        cy.get('#id_senha').type('123')
-        cy.get('#id_confirmar_senha').type('123')
-        cy.get('.btn').click();
-        cy.wait(1500)
-        cy.get(':nth-child(2) > a > .img-fluid').click()
-        cy.wait(1500)
-        cy.get('#ver-mais-lista-os').click()
-        cy.wait(1500)
-        cy.get('.btn').click()
-        cy.wait(1500)
-        cy.get(':nth-child(1) > a > .img-fluid').click()
-        cy.get('.card').should('exist')
+        cy.wait(2000);
+        cy.get('.card').should('exist');
     })
 })
